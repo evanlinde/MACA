190fb50625943f9ccaaadc8b556803152d5dfda0
Evan Linde <elinde@okstate.edu>
2018-07-27 10:40:47 -0500

    handle multiple notebooks

--- convert_notebook.sh
+++ convert_notebook.sh
@@ -4,11 +4,34 @@
 # web display and print-friendly PDFs.
 # 
 
-title="\"Converting MACA Data for Envision and SWAT Models\""
+file_basename="$1"
+
+declare -A titles
+titles=(
+  [MACA_to_Envision_SWAT]="\"Converting MACA Data for Envision and SWAT Models\""
+  [MACA_to_SWAT]="\"Converting MACA Data for SWAT Models\""
+  [AWR]="\"Converting MACA Data for Envision and SWAT Models - AWR\""
+)
+declare -A modify_dates
+modify_dates=(
+  [MACA_to_Envision_SWAT]="2017-10-19"
+  [MACA_to_SWAT]="2017-11-21"
+  [AWR]="2018-07-26"
+)
+declare -A pdf
+pdf=(
+  [MACA_to_Envision_SWAT]=1
+  [MACA_to_SWAT]=1
+  [AWR]=0
+)
+
+#title="\"Converting MACA Data for Envision and SWAT Models\""
+title=${titles[${file_basename}]}
 author="\"Evan Linde\""
 institution="\"Oklahoma State University\""
-today="\"$(date +'%B %-d, %Y')\""
-file_basename="MACA_to_Envision_SWAT"
+#today="\"$(date +'%B %-d, %Y')\""
+[[ ${modify_dates[${file_basename}]+1} ]] && today="\"$(date --date="${modify_dates[${file_basename}]}" +'%B %-d, %Y')\"" || today="\"$(date +'%B %-d, %Y')\""
+#file_basename="MACA_to_Envision_SWAT"
 
 # Convert the notebook to markdown
 # The markdown file will be ${file_basename}.md
@@ -46,11 +69,13 @@ sed -n '/^\#\ Introduction/,${s/^\!\[\(.*\)\](attachment:.*)\ *$/\!\[\1\]\(\1\)/
 rm ${file_basename}.md
 
 # Make HTML page
-pandoc -f markdown_github+tex_math_dollars+yaml_metadata_block doc/${file_basename}.md --css doc/pandoc.css --toc -s --mathml -o ${file_basename}.html
+pandoc -f markdown_github+tex_math_dollars+yaml_metadata_block doc/${file_basename}.md --css doc/pandoc.css --toc -s --mathjax -o ${file_basename}.html
 
-# Make a PDF with default margins
-pandoc -f markdown_github+tex_math_dollars+yaml_metadata_block --toc --listings -H doc/listings-setup.tex doc/${file_basename}.md -o doc/${file_basename}.pdf
+# Only make PDFs for some documents
+if [[ ${pdf[${file_basename}]} -eq 1 ]]; then
+  # Make a PDF with default margins
+  pandoc -f markdown_github+tex_math_dollars+yaml_metadata_block --toc --listings -H doc/listings-setup.tex doc/${file_basename}.md -o doc/${file_basename}.pdf
 
-# Make a PDF with smaller margins
-pandoc -f markdown_github+tex_math_dollars+yaml_metadata_block --toc --listings -H doc/listings-setup.tex -V geometry:"left=2.5cm, top=2cm, right=2.5cm, bottom=2cm" doc/${file_basename}.md -o doc/${file_basename}_small_margins.pdf
-
+  # Make a PDF with smaller margins
+  pandoc -f markdown_github+tex_math_dollars+yaml_metadata_block --toc --listings -H doc/listings-setup.tex -V geometry:"left=2.5cm, top=2cm, right=2.5cm, bottom=2cm" doc/${file_basename}.md -o doc/${file_basename}_small_margins.pdf
+fi
